791b7966696cd86c0e43dc13e8f93c19ba3f6a71
paulokiki <vieiralinspaulo@gmail.com>
2020-08-29 10:36:38 -0300

    tela de login

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/estiloLogin.css" media="screen" />
+
+	<meta charset="utf-8">
+	<title>Tela de Login</title>
+	
+</head>
+<body>
+
+	<form class="container">
+
+		<h1>LOGIN</h1>
+		<input class="input" type="text" placeholder="Digite o nome de usuário">
+		<input class="input" type="password" placeholder="Digite sua senha">
+		<input class="botao" type="submit" value="login">
+
+
+	</form>
+
+
+</body>
+</html>
